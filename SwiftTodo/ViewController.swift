4cbbb34505862ccc852d0d377771fb714f8a37d4
Akira Ishii <exilias774@gmail.com>
2014-06-03 15:29:59 +0900

    Initial Commit

---
+++ SwiftTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTodo
+//
+//  Created by 石井晃 on 2014/06/03.
+//  Copyright (c) 2014年 exilias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
