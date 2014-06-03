4abc484e4dc58916685e8ec77a4fd4dbcdb25ca6
Akira Ishii <exilias774@gmail.com>
2014-06-03 16:38:36 +0900

    Bridging Headerを追加し、設定した

---
+++ SwiftTodo/SwiftTodo-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  SwiftTodo-Bridging-Header.h
+//  SwiftTodo
+//
+//  Created by 石井晃 on 2014/06/03.
+//  Copyright (c) 2014年 exilias. All rights reserved.
+//
+
+
+#import "CoreData+MagicalRecord.h"
+#import "Todo.h"
