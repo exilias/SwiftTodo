4ef70673c757e870952bc4a47ebb2a223662de37
Akira Ishii <exilias774@gmail.com>
2014-06-03 16:05:38 +0900

    IBAction, IBOutletを張った

--- SwiftTodo/ViewController.swift
+++ SwiftTodo/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    
+    @IBOutlet var tableView : UITableView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTouchAddButton(sender : AnyObject) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
