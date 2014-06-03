bcfbbbf42d1378f3257ebf652a6f5ba2747149cc
Akira Ishii <exilias774@gmail.com>
2014-06-03 15:43:17 +0900

    pod install MagicalRecord

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, "7.0"
+
+target "SwiftTodo" do
+
+pod 'MagicalRecord'
+
+end
+
+target "SwiftTodoTests" do
+
+end
+
---
+++ Pods/Pods-SwiftTodo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftTodo : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftTodo
+@end
